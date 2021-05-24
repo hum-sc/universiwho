52d4b5f2c6697cbeda37faf2254c322543e909ee
hum-sc <humbertoantsc@gmail.com>
2021-05-24 14:28:12 -0500

    todo bien

---
+++ src/components/content.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Schools from './schools'
+function Content(){
+    return(<>
+        <Schools/>
+    </>);
+}
+export default Content;
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header (){
+    return(
+    <div>
+        <h3>UniverWhat ?</h3>
+    </div>
+    );
+}
+export default Header;
